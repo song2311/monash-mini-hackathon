175288b033fba2fcdcabd29c4da97fe218574695
shun <songshunnew@hotmail.com>
2020-08-27 14:43:17 +0800

    Completed functionality of find_qualified function which will find

--- driver.py
+++ driver.py
@@ -2,4 +2,6 @@ from lowest_cost import lowest_cost
 
 if __name__ == "__main__":
     cost=lowest_cost('team.json','tasks.json')
-    cost.find_qualified()
+    qualified=cost.find_qualified()
+    for member in qualified:
+        print(member)
--- lowest_cost.py
+++ lowest_cost.py
@@ -9,21 +9,46 @@ class lowest_cost:
         
     #this method reads a json file and stores it in an variable, it will return a json string
     def read_json(self,json_file):
-        with open('team.json') as f:
+        with open(json_file) as f:
             json_string = json.load(f)
         f.close()
         return json_string
 
-
+    #this function will find members in the team that are qualified for the task
     def find_qualified(self):
-        task=self.__team[0]['Skills']
-        print(task)
-        for skill in task:
-            print(task[skill])
-
+        task=self.__tasks[0]['Skills']
+        team= self.__team
+        print("Tasks:",task)
+        for member in team:
+            for skill in task:
+                qualify=False
+                for mem_skill in member['Skills']:
+                    if mem_skill==skill:
+                        skill_level=member['Skills'][skill]
+                        skill_req=task[skill]
+                        #if member meets skill requirements set qualify to true and break loop
+                        if skill_level>=skill_req:
+                            qualify=True
+                            break
+                #exit loop early if member did not meet one of the skill requirements
+                if qualify==False:
+                    break
+            #only add member to qualified list if member meets all requirements
+            if qualify==True:
+                self.qualified.append(member)
+        return self.qualified
+                    
+            
     
         
     
 
 
     
+#for skill in task:
+    #prints the rank of skill
+        #print(task[skill])
+#for member in team:
+    #print(member['Name'])
+    #for skill in member['Skills']:
+         #print(skill,":",member['Skills'][skill])
