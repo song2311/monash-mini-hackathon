71878030a5399ff3d7a4285a3e40e9860797d317
shun <songshunnew@hotmail.com>
2020-08-27 14:06:03 +0800

    Added a driver file and changed instance variable visibility to private

---
+++ driver.py
@@ -0,0 +1,4 @@
+from lowest_cost import lowest_cost
+
+cost=lowest_cost('team.json','tasks.json')
+cost.print_json(cost.team)
--- lowest_cost.py
+++ lowest_cost.py
@@ -3,8 +3,8 @@ import json
 class lowest_cost:
 
     def __init__(self, team, tasks):
-        self.team = self.read_json(team)
-        self.tasks = self.read_json(tasks)
+        self.__team = self.read_json(team)
+        self.__tasks = self.read_json(tasks)
         
     #this method reads a json file and stores it in an variable, it will return a json string
     def read_json(self,json_file):
@@ -12,8 +12,6 @@ class lowest_cost:
             json_string = json.load(f)
         f.close()
         return json_string
-          
-    
 
     
         
