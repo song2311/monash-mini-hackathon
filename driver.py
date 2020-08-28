a2de1c8abc9a6f375bb2a0d4d48c3111fed6cce3
shun <songshunnew@hotmail.com>
2020-08-28 14:09:49 +0800

    changed function name of find qualified to filter skill to better reflect the functionality

--- driver.py
+++ driver.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     find_rank_class=find_rank('team.json','tasks.json')
     
     for task in find_rank_class.tasks():
-        filter_skill=find_rank_class.find_qualified(task['Skills'])
+        filtered_skill=find_rank_class.filter_skill(task['Skills'])
         print("Tasks:",task)
         print("\nRank by skill")
-        filter_skill=find_rank_class.rank_by_skill(task,filter_skill)
+        filtered_skill=find_rank_class.rank_by_skill(task,filter_skill)
         count=0
         for member in filter_skill:
             print("Rank",count,":",member)
             count+=1
-            
+        
         print("\nRank by cost\n")
