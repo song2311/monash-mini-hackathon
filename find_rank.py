57fcc5407873a65d0c7ee5b12a61ff1e8ceeee9d
shun <songshunnew@hotmail.com>
2020-08-30 22:00:38 +0800

    reverted code and added comments to the code

--- find_rank.py
+++ find_rank.py
@@ -1,15 +1,12 @@
 """
 Author: 
 """
-
-
 import json
 from performance import decorator
 import time
 
 class find_rank():
     total_times = []
-
     """
     Class constructor for find_rank
     Initializes the total_times to empty list
@@ -23,10 +20,11 @@ class find_rank():
         self.__call_count = 0
         self.__team = team
         self.__tasks = tasks
-        self.__meet_req=[]
-        self.__fail_req=[]
+        self.__meet_req_cost=[]
+        self.__meet_req_skill=[]
+        self.__fail_req_cost=[]
+        self.__fail_req_skill=[]
         self.__run_time = 0
-    
 
     """
     Function that checks if the json file is empty after formatting it into a list  
@@ -35,10 +33,8 @@ class find_rank():
                         Worst case = O(1) time complexity
     Return: true or false if the team or tasks list is empty
     """
-    @decorator(total_times)
     def is_empty(self):
         return len(self.__team[0]) == 0 or len(self.__tasks[0]) == 0
-
 
     """
     Function to return the list of tasks extracted from the task json file
@@ -53,32 +49,7 @@ class find_rank():
             return []
         self.__call_count += 1
         return self.__tasks
-
-
-    """
-    Function to return the list of members that meets the requirements of a task
-    Arguments:          None
-    Time complexity:    Best case = O(1) time complexity
-                        Worst case = O(1) time complexity
-    Return: the list of members that meets the requirements of a task
-    """
-    @decorator(total_times)
-    def meet_req(self):
-        self.__call_count += 1
-        return self.__meet_req
-
-    """
-    Function to return the list of members that do not meet the requirements of a task
-    Arguments:          None
-    Time complexity:    Best case = O(1) time complexity
-                        Worst case = O(1) time complexity
-    Return: the list of members that do not meet the requirements of a task
-    """
-    @decorator(total_times)
-    def fail_req(self):
-        self.__call_count += 1
-        return self.__fail_req
-    
+               
     """
     Function that parses the json file, populate it in a dictionary and returns it
     Arguments:          json_file, the file to parse into a dictionary
@@ -86,6 +57,7 @@ class find_rank():
                         Worst case = O(N) time complexity
     Return: the list of tasks
     """
+    #this method reads a json file and stores it in an variable, it will return a json string
     @decorator(total_times)
     def read_json(self,json_file):
         self.__call_count += 1
@@ -93,26 +65,29 @@ class find_rank():
             json_string = json.load(f)
         f.close()
         return json_string
-
+    
     """
-    Function that filter skills unrelated to the tasks
+    Function that filter skills unrelated to the tasks, and assigns them to different lists based on whether they meet the skill requirements
     A member without any skill required by a task will have an empty skill dictionary
     Arguments:          task, the task to filter with
-    Time complexity:    Best case = 
-                        Worst case = 
+    Time complexity:    Best case =  O(n*m), where n is the number of json elements in team json and m is the number of elements in tasks json
+                        Worst case = O(n*m), where n is the number of json elements in team json and m is the number of elements in tasks json
     Return: the list of tasks
     """
+    #this function will filter skills unrelated to the tasks, a member without any skill required by a task will have an empty skill dictionary
     #Worst time complexity is O(n*m), where n is the number of json elements in team json and m is the number of elements in tasks json
     @decorator(total_times)
     def filter_skill(self,task):
         if self.is_empty():
             return
         self.__call_count += 1
-        #empty list before filtering skill because it is different for each task
-        self.__meet_req=[]
-        self.__fail_req=[]
+        #empty lists before filtering skill because it is different for each task
+        self.__meet_req_cost=[]
+        self.__meet_req_skill=[]
+        self.__fail_req_cost=[]
+        self.__fail_req_skill=[]
         for member in self.__team:
-            qualify=True
+            meet_req=True
             #the Skills dictionary will be empty if the member has 0 skill for the task
             member_dict={'Name':member['Name'],'Skills':{}}
             for skill in task:
@@ -120,33 +95,39 @@ class find_rank():
                     #check if a member has the required skill
                     if member['Skills'][skill]:
                         member_dict['Skills'][skill]=member['Skills'][skill]
-                
+                    #member does not qualify for the task
+                    if member['Skills'][skill]<task[skill]:
+                        meet_req=False
                 except KeyError:
-                    qualify=False
                     #if skill cannot be found just continue
                     continue
             #only insert those that at least have all the required skills into the met_req list because those members will be higher rank than the others
-            if len(member_dict['Skills'])==len(task) and qualify:
-                self.__meet_req.append(member_dict)
+            if len(member_dict['Skills'])==len(task):
+                if meet_req:
+                    self.__meet_req_cost.append(member_dict)
+                    self.__meet_req_skill.append(member_dict)
+                else:
+                    self.__fail_req_cost.append(member_dict)
+                    self.__fail_req_skill.append(member_dict)
             else:
-                self.__fail_req.append(member_dict)
+                self.__fail_req_cost.append(member_dict)
+                self.__fail_req_skill.append(member_dict)
+                
 
-    
     """
-    Function that takes in an array that contains the members and the filtered skill 
+    Function that takes in a list that contains the task and it will calculate the rank of members in self.__meet_req_skill based on the difference of skill level of member and skill level requirements
+    of a task, higher score means higher rank
     and sorts the rank of each member by the skill level 
     Arguments:          task, the task to check with skills of members in the team
     Time complexity:    Best case = 
                         Worst case = 
     Return: the list of members ranked by skill
     """
-    #Worst time complexity is O(n*m), where n is the number of json elements in team json and m is the number of elements in tasks json
     @decorator(total_times)
     def rank_by_skill(self,task):
         if self.is_empty():
             return
-    
-        for member in self.__meet_req:
+        for member in self.__meet_req_skill:
             #count the total differnce of all skill level and skill level requirements, lower means higher rank
             total_score=0
             for skill in task['Skills']:
@@ -156,7 +137,7 @@ class find_rank():
                 total_score+=(skill_level-skill_req)
                 member["Skill difference"]=total_score
         
-        for member in self.__fail_req:
+        for member in self.__fail_req_skill:
             #count the total differnce of all skill level and skill level requirements, lower means higher rank
             total_score=0
             for skill in task['Skills']:
@@ -169,19 +150,19 @@ class find_rank():
                     #minus the skill requirements if the member does not have the skills
                     total_score-=skill_req
             member["Total score"]=total_score
-        try:   
-            self.__meet_req.sort(key=lambda e: e["Skill difference"],reverse=True)
-            self.__fail_req.sort(key=lambda e: e['Total score'],reverse=True)
+        try:
+            #reverse sort because a higher score means better skill
+            self.__meet_req_skill.sort(key=lambda e: e["Skill difference"],reverse=True)
+            self.__fail_req_skill.sort(key=lambda e: e['Total score'],reverse=True)
         except KeyError:
             pass
         
         self.__call_count += 1
-        return self.__meet_req+self.__fail_req
-
-
+        return self.__meet_req_skill+self.__fail_req_skill
+    
     """
-    Function that takes in an array that contains the members and the filtered skill 
-    and sorts the rank of each member by the cost level 
+    Function that takes in a list that contains the task and it will calculate the rank of members in self.__meet_req_skill based on the difference of skill level of member and skill level requirements
+    of a task, self.__fail_req_skill uses a different formula for calculation where members without a skill will definitely have a lower ranking.
     Arguments:          task, the task to check with skills of members in the team
     Time complexity:    Best case = 
                         Worst case = 
@@ -192,20 +173,17 @@ class find_rank():
         if self.is_empty():
             return
     
-        for member in self.__meet_req:
+        for member in self.__meet_req_cost:
             #count the total differnce of all skill level and skill level requirements, lower means higher rank
             total_score=0
             for skill in task['Skills']:
                 skill_req=task['Skills'][skill]
                 skill_level=member['Skills'][skill]
                 #find the balance to see how overqualified a member is in relation to the skill requirements
-                if skill_level>=skill_req:
-                    total_score+=(skill_level-skill_req)
-                else:
-                     total_score+=(skill_req-skill_level)
+                total_score+=(skill_level-skill_req)
                 member["Skill difference"]=total_score
         
-        for member in self.__fail_req:
+        for member in self.__fail_req_cost:
             #count the total differnce of all skill level and skill level requirements, lower means higher rank
             total_score=0
             for skill in task['Skills']:
@@ -213,25 +191,20 @@ class find_rank():
                     skill_req=task['Skills'][skill]
                     skill_level=member['Skills'][skill]
                     #find the balance to see how overqualified a member is in relation to the skill requirements
-                    if skill_level>=skill_req:
-                        total_score+=(skill_level-skill_req)
-                    #check how much skill the member requires to meet the skill requirements
-                    else:
-                        total_score+=(skill_req-skill_level)
+                    total_score+=abs(skill_req-skill_level)
                 except KeyError:
                     #minus the skill requirements if the member does not have the skills
-                    total_score+=skill_req
+                    total_score+=5
             member["Total score"]=total_score
         try:   
-            self.__meet_req.sort(key=lambda e: e["Skill difference"])
-            self.__fail_req.sort(key=lambda e: e['Total score'])
+            self.__meet_req_cost.sort(key=lambda e: e["Skill difference"])
+            self.__fail_req_cost.sort(key=lambda e: e['Total score'])
         except KeyError:
             pass
         
         self.__call_count += 1
-        return self.__meet_req+self.__fail_req
+        return self.__meet_req_cost+self.__fail_req_cost
 
-    
     """
     Function that returns the number of times calls have been made in this class 
     Arguments:          None
@@ -273,20 +246,19 @@ class find_rank():
             ranked_list=self.rank_by_skill(task)
             count=0
             for member in ranked_list:
-                print("Rank",count,":",member)
+                print("Rank",count,":",member['Name'],"Skills:",member['Skills'])
                 count+=1
                 
             print("\nRank by cost\n")
             ranked_list=self.rank_by_cost(task)
             count=0
             for member in ranked_list:
-                print("Rank",count,":",member)
+                print("Rank",count,":",member['Name'],"Skills:",member['Skills'])
                 count+=1
             
             print("")
         end = time.time()
         self.__run_time = (end-start) * 1000
-
 
     """
     Function that logs the overall performance of the class into a text file
@@ -310,13 +282,3 @@ class find_rank():
             file.write("Minimum call elapsed time in milliseconds : %f" %min(find_rank.total_times))
         file.write("\n")
         file.close()
-
-
-
-
-
-
-    
-                
-   
-                    
